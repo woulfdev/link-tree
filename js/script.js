803d769983d73d7ab926c19800e41aaf0e1d85e8
devment Studio <kaile0426@gmail.com>
2023-11-08 19:26:03 +0100

    Added Pop Up fade in

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('popup-container').addEventListener('click', (event) => {
+        document.getElementById('popup-container').style.display = 'none'
+    })
+})
+
 function openLink(link) {
     window.open(link, '_blank');
 }
 
 function openPopUp() {
-    
+    document.getElementById('popup-container').style.display = 'block'
 }
